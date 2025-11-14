2f092e091be6144dd50681812ad0fe2e9cfd0293
YaoZhuo <yaozhuo@buaa.edu.cn>
2025-11-14 15:33:55 +0800

    update

--- script/enable-virtual-display.sh
+++ script/enable-virtual-display.sh
@@ -12,6 +12,6 @@ echo "No monitor detected, starting virtual display."
 
 # 创建虚拟屏幕
 xrandr --setprovideroutputsource 1 0 2>/dev/null
-xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1083 1088 1120 -hsync +vsync
-xrandr --addmode VIRTUAL1 1920x1080_60.00
-xrandr --output VIRTUAL1 --mode 1920x1080_60.00
+xrandr --newmode "1280x720_60.00"  172.80  1920 2040 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode VIRTUAL1 1280x720_60.00
+xrandr --output VIRTUAL1 --mode 1280x720_60.00
