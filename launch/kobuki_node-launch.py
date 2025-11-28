ddb3347e675a15384198cc8efe73e6dcb84008cf
yaozhuo <1521232476@qq.com>
2025-11-28 16:57:06 +0800

    update

--- launch/kobuki_node-launch.py
+++ launch/kobuki_node-launch.py
@@ -3,24 +3,36 @@ import os
 import ament_index_python.packages
 import launch
 import launch_ros.actions
+from launch.substitutions import LaunchConfiguration
 
 import yaml
 
 
 def generate_launch_description():
-    share_dir = ament_index_python.packages.get_package_share_directory('kobuki_node')
+    odom_frame = LaunchConfiguration('odom_frame', default='odom')
+    base_frame = LaunchConfiguration('base_frame', default='base_frame')
+
+
+    share_dir = ament_index_python.packages.get_package_share_directory('lamapf_and_gazebo')
     # There are two different ways to pass parameters to a non-composed node;
     # either by specifying the path to the file containing the parameters, or by
     # passing a dictionary containing the key -> value pairs of the parameters.
     # When starting a *composed* node on the other hand, only the dictionary
     # style is supported.  To keep the code between the non-composed and
     # composed launch file similar, we use that style here as well.
+
     params_file = os.path.join(share_dir, 'config', 'kobuki_node_params.yaml')
+
     with open(params_file, 'r') as f:
         params = yaml.safe_load(f)['kobuki_ros_node']['ros__parameters']
     kobuki_ros_node = launch_ros.actions.Node(package='kobuki_node',
                                               executable='kobuki_ros_node',
                                               output='both',
-                                              parameters=[params])
+                                              parameters=[params,
+                                                            {        # 覆盖 YAML 中的键值
+                                                            'odom_frame': odom_frame,
+                                                            'base_frame': base_frame,
+                                                            }],
+                                              )
 
     return launch.LaunchDescription([kobuki_ros_node])
